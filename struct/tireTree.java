ed8f15c2ddbb391decf40a5da33688099d0fde1f
oszn <1091756452@qq.com>
2021-02-05 11:34:21 +0800

    tire

---
+++ struct/tireTree.java
@@ -0,0 +1,44 @@
+package partten.Struck;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class tireTree {
+    int endCount;
+    int prefixCount;
+    char self;
+    private void _insert(String word,int index){
+        if(index==word.length()){
+            prefixCount++;
+            endCount++;
+            return;
+        }
+        this.prefixCount++;
+        int x=word.charAt(index)-'a';
+        if(sons[x]==null) {
+            sons[x] = new tireTree();
+            sons[x].self=word.charAt(index);
+        }
+        sons[x]._insert(word,index+1);
+
+    }
+    public void insert(String word){
+        _insert(word,0);
+    }
+    tireTree[] sons;
+    public tireTree(){
+        sons=new tireTree[26];
+        this.prefixCount=0;
+        this.endCount=0;
+    }
+
+    public static void main(String[] args) {
+        tireTree t=new tireTree();
+        t.insert("his");
+        t.insert("her");
+        t.insert("hello");
+        t.insert("do");
+        int a=10;
+        System.out.println("error");
+    }
+}
